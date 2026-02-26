004e4b2bbcbda06687f5c77ffad40c88790bd2e3
parthcodes90 <ParthPatil.comp@sitrc.org>
2026-02-26 19:58:55 +0530

    Update vite.config.js to reference new src location

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': '/web/frontend/src',
     },
   },
-})
+});
